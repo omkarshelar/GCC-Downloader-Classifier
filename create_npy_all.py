aef5725680b1c5f0dde4c9b6951bdc0b78767023
Omkar Waghmare <45145849+waghmareomkar@users.noreply.github.com>
2020-02-19 15:15:09 -0500

    Add files via upload

--- create_npy_all.py
+++ create_npy_all.py
@@ -1,12 +1,14 @@
 #equivalent to id2word.npy and word2id.npy ,train_captions and train_images
 
 import numpy as np
-#import cv2
+import cv2
 import glob
+
+import pickle
 
 encoding = {} #dictionary
 #Open the file
-with open("captions_Dog.txt", 'r') as my_File:
+with open("C:/Users/omkar waghmare/Desktop/text to image/text-to-image-master/captions.txt", 'r') as my_File:
     #Read the files
     contents = my_File.readlines()
     my_File.seek(0)
@@ -28,44 +30,50 @@ for a in unique_words:
 encoding["<PAD>"]=i
 
 #creating train_captions.npy
-final_array = np.empty(shape = (7416,1)) 
+final_array = np.empty(shape = (8106,1)) 
 final_list = list()
-j=0
+
 for line in contents:
     line = line.replace("\n" , "")
     listOfWords = list()
     i = 0
     for word in line.split(" "):
-        listOfWords.append(str(encoding[word]))
+        listOfWords.append(np.str_(encoding[word]))
     
+    while len(listOfWords) < 20:
+        listOfWords.append(np.str_(encoding["<PAD>"]))
+
+    # print(listOfWords) # Works
+
+
     listOflist = list()
     while i < 5:
         listOflist.append(listOfWords)
         i+=1
-    
-    final_array[j] = listOflist
 
     final_list.append(listOflist)
-    
-#final_array = np.array(final_list)
-#final_array = final_array.reshape(7416, 5)
-#print(final_list[0:2])
-
-print(".SHAPETEST",final_array.shape)
-
-
-#print(len(final_list))
+    #print(final_list)
 
     #np.append(arr = final_array , values = listOflist, axis=0)
-#final_array = final_array.flatten(order='C')
+
+#final_final_array = np.array(final_list)
+np.save("train_captions_dog.npy",final_list)
+print(len(final_list))
+print(type(final_list[0]))
 
 
-#np.insert(arr=final_array, obj = 0 ,values = final_list, axis=0)
-#print(len(final_list))
+
+with open("C:/Users/omkar waghmare/Desktop/text to image/text-to-image-master/captions_dog.txt", "wb+") as fp:   #Pickling
+    pickle.dump(final_list, fp)
+
+
+'''
+np.insert(arr=final_array, obj = 0 ,values = final_list, axis=0)
+print(len(final_list))
 np.save("train_captions_dog.npy",final_array)
 test = np.load("train_captions_dog.npy")
 print(".SHAPETEST",test.shape)
-exit()
+'''
 
 np.save("word2id_dog.npy",encoding)
 print("Saved words to ID")
@@ -75,16 +83,15 @@ id2word = {v: k for k, v in encoding.items()}
 np.save("id2word_dog.npy",id2word)
 print("Saved ID to word")
 
-
-
 #creating train_images
 X_data = []
-files = glob.glob ("C:/Users/omkar waghmare/Desktop/text to image/text-to-image-master/images_dog/*.jpg")
+files = glob.glob ("C:\\Users\\omkar waghmare\\Desktop\\text to image\\text-to-image-master\\imgs_dog_V2\\*.jpg")
 for myFile in files:
     #print(myFile)
     image = cv2.imread (myFile) 
     X_data.append (image)
 
-np.save('train_images_dog.npy',np.array(X_data))
+print(len(X_data))
+np.save('C:/Users/omkar waghmare/Desktop/text to image/text-to-image-master/train_images_dog.npy',np.array(X_data))
 print("Saved Images as numpy")
 print("SAvedddddddddddddddddddddddddd")
